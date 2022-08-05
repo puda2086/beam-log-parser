1b1ecd4de5bf2ccc095b045862c65408878ac779
Pulina <pulinau@gmail.com>
2022-08-05 15:34:22 +0530

    Add validation to input file option

--- src/main/java/org/example/logscanner/options/LogScannerOptions.java
+++ src/main/java/org/example/logscanner/options/LogScannerOptions.java
@@ -3,10 +3,12 @@ package org.example.logscanner.options;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.Validation;
 
 public interface LogScannerOptions extends PipelineOptions {
 
     @Description("Filename or file pattern to read logs from.")
+    @Validation.Required
     String getInputFile();
 
     void setInputFile(String value);
